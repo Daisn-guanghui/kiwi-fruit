1492f87f767a0ed63a9090943f5fcf7b138ecbf4
daisn <guanghui1.zhang@outlook.com>
2023-09-10 16:37:22 +0800

    初始化

---
+++ src/main/java/com/daisn/kiwifruit/KiwiFruitApplication.java
@@ -0,0 +1,13 @@
+package com.daisn.kiwifruit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiwiFruitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KiwiFruitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/daisn/kiwifruit/KiwiFruitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daisn.kiwifruit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KiwiFruitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
